96e4a72f0bc4ef0f276dcd3dbcfa0fd7b27b0245
chatfly <oiamigopao@outlook.com>
2025-09-03 21:46:00 -0300

    feat: 'Conecta' a infrastructure junto com o core utilizando gateways

---
+++ src/main/java/dev/java10x/EventClean/core/gateway/EventoGateway.java
@@ -0,0 +1,4 @@
+package dev.java10x.EventClean.core.gateway;
+
+public interface EventoGateway {
+}
---
+++ src/main/java/dev/java10x/EventClean/infrastructure/gateway/EventoRepositoryGateway.java
@@ -0,0 +1,7 @@
+package dev.java10x.EventClean.infrastructure.gateway;
+
+import dev.java10x.EventClean.core.gateway.EventoGateway;
+
+public class EventoRepositoryGateway implements EventoGateway {
+
+}
