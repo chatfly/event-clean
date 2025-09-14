b11921958ca14b5f3855fdebad20e4a3145dbca0
chatfly <oiamigopao@outlook.com>
2025-09-14 16:01:52 -0300

    feat: Cria uma endpoint para busca de Evento por identificador único e cria uma Bean para a filtragem.

--- src/main/java/dev/java10x/EventClean/infrastructure/beans/BeanConfiguration.java
+++ src/main/java/dev/java10x/EventClean/infrastructure/beans/BeanConfiguration.java
@@ -2,10 +2,7 @@ package dev.java10x.EventClean.infrastructure.beans;
 
 import dev.java10x.EventClean.core.entities.Evento;
 import dev.java10x.EventClean.core.gateway.EventoGateway;
-import dev.java10x.EventClean.core.usecases.buscarEventoUsecase;
-import dev.java10x.EventClean.core.usecases.buscarEventoUsecaseImpl;
-import dev.java10x.EventClean.core.usecases.criarEventoUsecase;
-import dev.java10x.EventClean.core.usecases.criarEventoUsecaseImpl;
+import dev.java10x.EventClean.core.usecases.*;
 import dev.java10x.EventClean.infrastructure.gateway.EventoRepositoryGateway;
 import dev.java10x.EventClean.infrastructure.mapper.EventoEntityMapper;
 import dev.java10x.EventClean.infrastructure.persistence.EventoRepository;
@@ -28,6 +25,11 @@ public class BeanConfiguration {
     @Bean
     public EventoGateway eventoGateway(EventoRepository eventoRepository, EventoEntityMapper eventoEntityMapper){
         return new EventoRepositoryGateway(eventoRepository, eventoEntityMapper);
+    }
+
+    @Bean
+    public filtroIdentificadorUsecase filtroIdentificador(EventoGateway eventoGateway) {
+        return new filtroIdentificadorUsecaseImpl(eventoGateway);
     }
 
 }
--- src/main/java/dev/java10x/EventClean/infrastructure/persistence/EventoRepository.java
+++ src/main/java/dev/java10x/EventClean/infrastructure/persistence/EventoRepository.java
@@ -1,6 +1,8 @@
 package dev.java10x.EventClean.infrastructure.persistence;
 
+import dev.java10x.EventClean.core.entities.Evento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventoRepository extends JpaRepository<EventoEntity, Long> {
+    Evento findByIdentificador(String identificador);
 }
